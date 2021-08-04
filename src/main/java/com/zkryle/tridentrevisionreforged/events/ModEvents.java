cb0d46dfbcf9e37d7027b1d46a2c2026c24ef636
zKryle <ciro20797@gmail.com>
2021-08-04 11:42:54 +0200

    Fixed Annoying bug in the repair recipe.

--- src/main/java/com/zkryle/tridentrevisionreforged/events/ModEvents.java
+++ src/main/java/com/zkryle/tridentrevisionreforged/events/ModEvents.java
@@ -27,15 +27,22 @@ public class ModEvents{
                 ItemStack stack = event.getLeft().copy();
                 int additionalcost = getEnchantmentCost( stack );
                 if(event.getLeft().getDamageValue() > 0 && event.getLeft().getDamageValue() <= 30){
+                    stack.setDamageValue( event.getLeft().getDamageValue() - 85 );
+                    event.setMaterialCost( 1 );
                     event.setCost( 2 + additionalcost );
                 }else if(event.getLeft().getDamageValue() >= 30 && event.getLeft().getDamageValue() <= 100){
+                    stack.setDamageValue( event.getLeft().getDamageValue() - 85 );
+                    event.setMaterialCost( 1 );
                     event.setCost( 5 + additionalcost );
                 }else if(event.getLeft().getDamageValue() >= 100 && event.getLeft().getDamageValue() <= 200){
+                    stack.setDamageValue( event.getLeft().getDamageValue() - 170 );
+                    event.setMaterialCost( 2 );
                     event.setCost( 7 + additionalcost );
                 }else if(event.getLeft().getDamageValue() >= 200){
+                    event.setMaterialCost( 3 );
+                    stack.setDamageValue( event.getLeft().getDamageValue() - 250 );
                     event.setCost( 10 + additionalcost );
                 }
-                stack.setDamageValue( event.getLeft().getDamageValue() - 85 );
                 event.setOutput( stack );
             }
         }
